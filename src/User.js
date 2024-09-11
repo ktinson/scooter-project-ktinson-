1cd93ddd69506d39cd1d60159fd73e085d39fa7a
ktinson <monikai.tinson@verizon.com>
2024-09-11 12:18:01 -0500

    added parameters

--- src/User.js
+++ src/User.js
@@ -2,6 +2,14 @@ const ScooterApp = require('./ScooterApp')
 const Scooter = require('./Scooter')
 class User {
   // User code here
+constructor(username, password, age, loggedIn){
+  this.username=username
+  this.password=password
+  this.age=age
+  loggedIn=false
+  this.loggedIn=loggedIn
 }
-
+login(password){}
+logout(){}
+}
 module.exports = User
