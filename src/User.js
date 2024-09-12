1fde1de1d29882b00c4c2a690cf1907bdb946095
ktinson <monikai.tinson@verizon.com>
2024-09-12 08:37:54 -0500

    Added user tests

--- src/User.js
+++ src/User.js
@@ -6,10 +6,18 @@ constructor(username, password, age){
   this.username=username
   this.password=password
   this.age=age
- 
   this.loggedIn=false
 }
-login(password){}
-logout(){}
+login(password){
+  if(password !== this.password){
+    throw Error('Incorrect password')
+  }
+  this.loggedIn = true
+  return `${this.username} logged in`
+}
+logout(){
+  this.loggedIn = false
+  return `${this.username} logged out`
+}
 }
 module.exports = User
--- tests/User.test.js
+++ tests/User.test.js
@@ -1,18 +1,52 @@
 const User = require('../src/User')
 
-const user = new User('Joe Bloggs', 'test123', 21)
 
 // User tests here
 describe('User property tests', () => {
+  let user
+  beforeEach(() => { user = new User('Joeymal', 'test123', 21)}) 
+
   // test username
   test('username should be a string', () => {
     expect(typeof user.username).toBe('string')
   })
+  test('Has correct username', () => {
+    expect(user.username).toBe('Joeymal')
+  })
   // test password
-
+  test('password should be a string', () => {
+    expect(typeof user.password).toBe('string')
+  })
+  test('Has correct password', () => {
+    expect(user.password).toBe('test123')
+  })
   // test age
+  test('age should be a number', () => {
+    expect(typeof user.age).toBe('number')
+  })
+  test('Has correct age', () => {
+    expect(user.age).toBe(21)
+  })
 })
 
 // test login
-
+describe('Has login and logout', () => {
+  let user
+  beforeEach(() => { user = new User('Joeymal', 'test123', 21)}) 
+  
+  test('has login false', () => {
+    expect(() => {user.login('test124');}).toThrow('Incorrect password')
+    expect(user.loggedIn).toEqual(false)
+  })
+  test('has login true', () => {
+    expect(user.login('test123')).toBe("Joeymal logged in")
+    expect(user.loggedIn).toBe(true)
+  })
+  test('has logout', () => {
+    const result = user.logout()
+    expect(user.loggedIn).toBe(false)
+    expect(result).toBe("Joeymal logged out")
+  })
+})
 // test logout
+
